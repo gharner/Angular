bbfe7cd1e172def7d5bc9677f137a79703cdd7e3
Greg Harner <accounts@yongsa.net>
2025-03-14 11:25:21 -0400

    attendance violation sandbox

--- src/app/components/experiments/experiments.component.ts
+++ src/app/components/experiments/experiments.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Firebase_Functions_Service } from 'src/app/services';
+import { Subscription } from 'rxjs';
+import { Firebase_Functions_Service, SandboxService } from 'src/app/services';
 
 @Component({
   selector: 'experiments',
@@ -7,15 +8,33 @@ import { Firebase_Functions_Service } from 'src/app/services';
   styleUrls: ['./experiments.component.css'],
 })
 export class Experiments implements OnInit {
-  constructor(private firebase: Firebase_Functions_Service) {}
+  private dataSubscription!: Subscription;
+  constructor(
+    private firebase: Firebase_Functions_Service,
+    private sandboxService: SandboxService
+  ) {}
 
   async ngOnInit() {
-    const schedules = this.firebase.getScheduelsNotClosed();
-    schedules.subscribe((schedule) => console.log(schedule));
+    console.info('ngOnInit');
+    this.refreshSchedules();
+  }
+
+  public async refreshSchedules() {
+    const testData = await this.sandboxService.getMAKSchedulesClosed();
+    console.log(testData);
+
+    const dataSubscription = this.firebase.getSchedulesNotClosed();
+    dataSubscription.subscribe((schedule) => console.log(schedule));
 
     // const account = await this.sandboxService.getActiveAccountsAsPromise();
     // console.log(account);
     // const programs = await this.sandboxService.getProgramsAsPromise();
     // console.log(programs);
+  }
+
+  ngOnDestroy() {
+    if (this.dataSubscription) {
+      this.dataSubscription.unsubscribe();
+    }
   }
 }
